487a21581530fa42e288a0ec7b740f75fcfd965d
Laercio Nogueira <laerciopesa30@hotmail.com>
2019-07-18 10:52:59 -0300

    create login

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, Button, StyleSheet } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import Logo from './../components/Logo'
+import Input from './../components/Input'
+
+export default props => {
+  const verifyLogin = () => {
+    Actions.pageHome()
+  }
+
+  return(
+    <View style={style.box}>
+      <Logo />
+      <Button title="Clicar"  onPress={() => verifyLogin()}/>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  box: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10
+  }
+})
