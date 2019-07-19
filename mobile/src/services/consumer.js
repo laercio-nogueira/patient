88890051dff1e3d5c0fe18356a774637b251e906
Laercio Nogueira <laerciopesa30@hotmail.com>
2019-07-19 13:41:43 -0300

    new services consumer

--- mobile/src/services/consumer.js
+++ mobile/src/services/consumer.js
@@ -2,6 +2,24 @@ import axios from 'axios'
 
 export default {
   getListConsultation () {
-    return axios.get('http://192.168.2.100:3000/start')
+    return axios.get('http://192.168.2.100:3000/consultation')
+  },
+  getConsultationById (id) {
+    return axios.get(`http://192.168.2.100:3000/consultation/${id}`)
+  },
+  saveNewConsultation (data) {
+    return axios.post('http://192.168.2.100:3000/consultation', data)
+  },
+  editConsultation (data, id) {
+    return axios.put(`http://192.168.2.100:3000/consultation/${id}`, data)
+  },
+  cancelConsultation (id) {
+    return axios.put(`http://192.168.2.100:3000/consultation/${id}/cancel`)
+  },
+  getListDoctor () {
+    return axios.get('http://192.168.2.100:3000/doctor')
+  },
+  getListHospital () {
+    return axios.get('http://192.168.2.100:3000/hospital')
   }
 }
