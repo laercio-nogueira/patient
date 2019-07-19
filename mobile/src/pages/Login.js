142d2c5465ed1839dff7a896a7541e706de471e5
Laercio Nogueira <laerciopesa30@hotmail.com>
2019-07-19 13:42:01 -0300

    field input in Login

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Button, StyleSheet } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import Logo from './../components/Logo'
+import { Container, Content, Form, Item, Input, Label } from 'native-base';
 
 export default props => {
   const verifyLogin = () => {
@@ -11,6 +12,16 @@ export default props => {
   return(
     <View style={style.box}>
       <Logo />
+        <View style={style.blockInput}>
+          <Item inlineLabel>
+            <Label>Email:</Label>
+            <Input />
+          </Item>
+          <Item inlineLabel last >
+            <Label>Senha:</Label>
+            <Input password secureTextEntry={true}/>
+          </Item>
+        </View>
       <Button title="Clicar"  onPress={() => verifyLogin()}/>
     </View>
   )
@@ -21,5 +32,13 @@ const style = StyleSheet.create({
     flex: 1,
     marginLeft: 10,
     marginRight: 10
+  },
+  blockInput: {
+    marginTop: 50,
+    marginBottom: 50,
+  },
+  input: {
+    marginTop: 20,
+    marginBottom: 20
   }
 })
