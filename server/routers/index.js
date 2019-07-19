361e8ddf28e769c3836086e715261a8509c3dcde
Laercio Nogueira <laerciopesa30@hotmail.com>
2019-07-19 13:40:26 -0300

    add new routers

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express.Router();
+const bodyParser = require('body-parser')
 const Consultation = require('../controller/ConsultationController')
+const Doctor = require('../controller/DoctorController')
+const Hospital = require('../controller/HospitalController')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.route('/start')
+app.route('/consultation')
   .get(Consultation.listAllConsultation)
-//   .post(Consultation.createConsultation)
-//   .put(Consultation.updateConsultation)
+  .post(Consultation.createConsultation)
+  .put(Consultation.updateConsultation)
+
+app.route('/consultation/:id')
+  .get(Consultation.getConsultationById)
+  .put(Consultation.updateConsultation)
+
+app.route('/consultation/:id/cancel')
+  .put(Consultation.cancelConsultation)
+
+app.route('/doctor')
+  .get(Doctor.listAllDoctor)
+
+app.route('/hospital')
+  .get(Hospital.listAllHospital)
 
 module.exports = app;
