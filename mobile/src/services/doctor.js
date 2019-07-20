4d1a1292af8396207089631d786f93117fca85e4
Laercio Nogueira <laerciopesa30@hotmail.com>
2019-07-19 22:25:25 -0300

    doctor service

--- mobile/src/services/doctor.js
+++ mobile/src/services/doctor.js
@@ -1,3 +1,9 @@
 import axios from 'axios'
+import store from '../store'
 
-export default {}
+export default {
+  getListPatients (id) {
+    const token = store.getState().user.token
+    return axios.get(`http://192.168.2.100:3000/patients`, { headers: { token } })
+  }
+}
