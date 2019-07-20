b8943b83c121112c8323f61b866782419d37688e
Laercio Nogueira <laerciopesa30@hotmail.com>
2019-07-19 22:26:38 -0300

    config home

--- mobile/src/pages/Home.js
+++ mobile/src/pages/Home.js
@@ -1,25 +1,52 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, StyleSheet, Button } from 'react-native'
 import { Actions } from 'react-native-router-flux'
-import Legend from './../components/Legend'
+import { connect } from 'react-redux'
+import Legend from '../components/Legend'
+import Header from '../components/Header'
 
-export default () => (
-  <View style={style.box}>
-    <Legend title="Bem vindo ao sistema" />
-    <Button 
-      style={style.card} 
-      title="Minhas Consultas" 
-      color="#aaa" 
-      onPress={() => Actions.pageListConsultation()}
-    />
-    <Button 
-      style={style.card} 
-      title="Agendar Consulta" 
-      color="#aaa"onPress={() => Actions.pageScheduleConsultation()}/>
-  </View>
-)
+const Home = props => {
+  if (props.user.account) {
+    return (
+      <View style={style.container}>
+        <Header />
+        <View style={style.box}>
+          <Legend title="Dashboard" />
+          <Button 
+            style={style.card} 
+            title="Meus Pacientes" 
+            color="#aaa" 
+            onPress={() => Actions.pageListPatients()}
+          />
+        </View>
+      </View>
+    )
+  } else {
+    return (
+      <View style={style.container}>
+        <Header />
+        <View style={style.box}>
+          <Legend title="Dashboard" />
+          <Button 
+            style={style.card} 
+            title="Minhas Consultas" 
+            color="#aaa" 
+            onPress={() => Actions.pageListConsultation()}
+          />
+          <Button 
+            style={style.card} 
+            title="Agendar Consulta" 
+            color="#aaa"onPress={() => Actions.pageScheduleConsultation()}/>
+        </View>
+      </View>
+    )
+  }
+}
 
 const style = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   box: {
     margin: 20,
     flex: 1
@@ -41,5 +68,21 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     textAlignVertical: "center",
     alignItems: "center"
+  },
+  profile: {
+    alignSelf: 'flex-end'
+  },
+  icon: {
+    color: 'blue',
+    marginLeft: 10,
+    borderColor: 'blue',
+    borderWidth: 1,
+    borderRadius: 50
   }
 })
+
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(Home)
